844a50e05870e6e341c30d4d1c0208fd1b2c9bc5
Sinsedrix <sinsedrix@gmail.com>
2025-03-14 00:17:22 +0100

    test fr

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -79,3 +79,48 @@ test('custom adjectives & nouns', () => {
 
   expect(customHaikunator.haikunate()).toMatch(/adjective-noun-\d{4}$/)
 })
+
+test('fr tests', () => {
+/**
+ * Adjectifs utilisés par haikunator
+ */
+const frAdjectives = [
+  'age', 'ancien', 'automnal', 'flottant', 'amer', 'noir', 'bleu', 'audacieux',
+  'large', 'casse', 'calme', 'froid', 'frais', 'cramoisi', 'boucle', 'humide',
+  'sombre', 'aube', 'delicat', 'divin', 'sec', 'vide', 'tombant', 'fantaisiste',
+  'plat', 'floral', 'parfume', 'glacial', 'doux', 'vert', 'cache', 'sacre',
+  'glace', 'joyeux', 'tardif', 'persistant', 'petit', 'vivant', 'long', 'chanceux',
+  'brumeux', 'matinal', 'boueux', 'muet', 'sans nom', 'bruyant', 'etrange', 'vieux',
+  'orange', 'patient', 'simple', 'poli', 'fier', 'violet', 'silencieux', 'rapide',
+  'rauque', 'rouge', 'agite', 'rugueux', 'rond', 'royal', 'brillant', 'strident',
+  'timide', 'silencieux', 'petit', 'enneige', 'doux', 'solitaire', 'etincelant', 'printanier',
+  'carre', 'abrupte', 'immobile', 'estival', 'super', 'sucre', 'palpitant', 'serre',
+  'minuscule', 'crepusculaire', 'errant', 'use', 'blanc', 'sauvage', 'hivernal', 'vaporeux',
+  'fletri', 'jaune', 'jeune'
+]
+
+/**
+ * Noms utilisés par haikunator
+ */
+const frNouns = [
+  'art', 'groupe', 'bar', 'base', 'oiseau', 'bloc', 'bateau', 'bonus',
+  'pain', 'brise', 'ruisseau', 'buisson', 'papillon', 'gateau', 'cellule', 'cerise',
+  'nuage', 'credit', 'obscurite', 'aube', 'rosee', 'disque', 'reve', 'poussiere',
+  'plume', 'champ', 'feu', 'luciole', 'fleur', 'brouillard', 'foret', 'grenouille',
+  'givre', 'clairiere', 'eclat', 'herbe', 'hall', 'chapeau', 'brume', 'coeur',
+  'colline', 'roi', 'laboratoire', 'lac', 'feuille', 'limite', 'mathematiques', 'prairie',
+  'mode', 'lune', 'matin', 'montagne', 'souris', 'boue', 'nuit', 'papier',
+  'pin', 'poesie', 'etang', 'reine', 'pluie', 'recette', 'resonance', 'riz',
+  'riviere', 'salade', 'scene', 'mer', 'ombre', 'forme', 'silence', 'ciel',
+  'fumee', 'neige', 'flocon', 'son', 'etoile', 'soleil', 'coucher de soleil',
+  'vague', 'terme', 'tonnerre', 'dent', 'arbre', 'verite', 'union', 'unite',
+  'violette', 'voix', 'eau', 'cascade', 'vague', 'fleur sauvage', 'vent', 'bois'
+]
+
+  const frHaikunator = new Haikunator({
+    adjectives: frAdjectives,
+    nouns: frNouns
+  })
+
+  console.log(frHaikunator.haikunate())
+})
